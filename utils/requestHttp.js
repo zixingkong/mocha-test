9b56fd5ec6bbede7e8e84b4e05a80ea366c35451
yayun.wang <yayun.wang@rootcloud.com>
2021-10-20 17:43:23 +0800

    fix: delete node_modules

--- utils/requestHttp.js
+++ utils/requestHttp.js
@@ -1,17 +1,18 @@
 import undici from 'undici';
 
 
-const client = new undici.Client('http://www.baidu.com')
+const client = new undici.Client('http://iam-identity-service.iot-dev.rootcloudapp.com')
 
 // async function httpRequestAsync(method = 'post', path, headers, reqBody, isJson = true) {
-async function httpRequestAsync(method = 'get', path='/',) {
+async function httpRequestAsync(method = 'post', path='/api/v1/auth/login',headers, reqBody, isJson) {
   const { statusCode, body } = await client.request({
     path,
-    method: method
+    method: method,
+    headers: headers,
+    reqBody: reqBody,
   });
-  const data = statusCode === 200 ? await body.json() : await body.text();
+  const data = await body.json();
   return { status: statusCode, data };
-
 }
 
 
