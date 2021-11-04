3cf202815aeb243ff42fd754eaba2fb5ebdd68b0
yayun.wang <yayun.wang@rootcloud.com>
2021-11-04 15:14:03 +0800

    fix: debug done

--- test/test_http.js
+++ test/test_http.js
@@ -1,12 +1,22 @@
 'use strict';
 
 import {httpRequestAsync } from '../utils/requestHttp.js'
-
+import { strict as assert } from 'assert';
 
 describe('test', function () {
 
     it('机构管理员登录', async function () {
-        let res = await httpRequestAsync()
-        console.log(res)
+        let headers = {'Content-Type': 'application/json'}
+        let reqBody = {
+            "grant_type": "password",
+            "username": "superAdmin_38140798",
+            "password": "123321",
+            "client_id": "web_app",
+            "client_secret": "changeit"
+        };
+        let res = await httpRequestAsync({method: 'POST',headers:headers,
+            path:'/api/v1/auth/login',reqBody:reqBody});
+        console.log(res);
+        assert.deepEqual(res.status,422);
     });
 });
--- utils/requestHttp.js
+++ utils/requestHttp.js
@@ -1,19 +1,19 @@
 import undici from 'undici';
 
 
-const client = new undici.Client('http://iam-identity-service.iot-dev.rootcloudapp.com')
+const client = new undici.Client('http://iam-identity-service.iot-qa.rootcloudapp.com');
 
 // async function httpRequestAsync(method = 'post', path, headers, reqBody, isJson = true) {
-async function httpRequestAsync(method = 'post', path='/api/v1/auth/login',headers, reqBody, isJson) {
-  const { statusCode, body } = await client.request({
-    path,
-    method: method,
-    headers: headers,
-    reqBody: reqBody,
-  });
-  const data = await body.json();
-  return { status: statusCode, data };
+async function httpRequestAsync({method = 'get', path, headers, reqBody, isJson = true} = {}) {
+    const {statusCode, body} = await client.request({
+        path: path,
+        method: method,
+        headers: headers,
+        reqBody: reqBody,
+    });
+    const data = await body.json();
+    return {status: statusCode, data};
 }
 
 
-export {httpRequestAsync}
+export {httpRequestAsync};
